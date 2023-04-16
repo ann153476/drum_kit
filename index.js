fcb4c80bc60029818f294a25aafdf079ec185ccd
Anka <103377387+ann153476@users.noreply.github.com>
2023-04-16 12:36:54 +0300

    all

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ const btns = document.querySelector(".box");
 
 function func(e) {
   let id = `#s${e.target.id.slice(3)}`;
-  //   id = e.target.id;
   let sound = document.querySelector(id);
   sound.play();
-  console.log(sound);
 }
 
 btns.addEventListener("click", func);
